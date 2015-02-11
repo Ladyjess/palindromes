871be21793b7a36f005f59a424c2479283b75b6d
jessica hori & sasha burchuk <student@epicodus.com>
2015-02-11 12:11:33 -0800

    Change spec file

--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -3,10 +3,10 @@ require 'rspec'
 
 describe "String#palindrome?" do
   it "compares the word to itself forwards and backwards to see if its a palindrome" do
-    expect(("hannah").palindrome?("hannah")).to eq true
+    expect("hannah".palindrome?("hannah")).to eq true
   end
 
   it 'compares word and returns false if not palindrome' do
-    expect(("won").palindrome?("happy")).to eq false
+    expect("won".palindrome?("happy")).to eq false
   end
 end
